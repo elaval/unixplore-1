37e365a1b786ae4ce6348bd3588125ddf6ef6f8e
Ernesto Laval <ernesto.laval@gmail.com>
2014-06-04 11:33:18 +0100

    Cambio de eje para r2 en cursos

--- Gruntfile.js
+++ Gruntfile.js
@@ -295,7 +295,8 @@ module.exports = function (grunt) {
             '*.html',
             'views/{,*/}*.html',
             'images/{,*/}*.{webp}',
-            'fonts/*'
+            'fonts/*',
+            'data/*'
           ]
         }, {
           expand: true,
